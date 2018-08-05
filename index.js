53dc50202c3e36a2b97a941139e4e93741d79069
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-05 13:50:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,11 @@ function multiply(a, b){
 function divide(a, b){
   return a / b
 }
-function inc(a){
-  return (a + 1)
+function inc(n){
+  return a + 1
 }
 function dec(a){
-  return(a - 1)
+  return a - 1
 }
 function makeInt(a){
   return(a)
